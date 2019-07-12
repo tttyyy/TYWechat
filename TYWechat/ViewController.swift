74a33526f72217347a891c95efaf34c50e8b857c
tangyujie <tangyujie@houyicloud.com>
2019-07-12 15:10:23 +0800

    Initial Commit

---
+++ TYWechat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TYWechat
+//
+//  Created by tangyj on 2019/7/12.
+//  Copyright © 2019 hycloud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
